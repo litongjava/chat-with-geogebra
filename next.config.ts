9b4147cf6ff70e6bdf48531efaac9badd473775c
ivory <contact@ivory.cafe>
2025-04-02 13:45:37 +0800

    init version 0.2.4

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ 	output: 'standalone',
+	trailingSlash: true,
+	assetPrefix: process.env.NODE_ENV === 'production' ? '/' : undefined,
+	images: {
+		unoptimized: true,
+	}
 };
 
 export default nextConfig;
